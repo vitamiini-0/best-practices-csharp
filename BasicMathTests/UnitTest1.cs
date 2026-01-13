6e01acbae2e29b181beee6c5f479a459eebd98f5
vitamiini-0 <228652369+vitamiini-0@users.noreply.github.com>
2026-01-13 13:43:34 +0200

    feat: add csharpier hook

--- BasicMathTests/UnitTest1.cs
+++ BasicMathTests/UnitTest1.cs
@@ -2,9 +2,11 @@
 
 namespace BasicMathTests;
 
-public class UnitTest1 {
+public class UnitTest1
+{
     [Fact]
-    public void Test_Add() {
+    public void Test_Add()
+    {
         var math = new MathEngine();
         var result = math.Add(1, 3);
         Assert.Equal(4, result);
